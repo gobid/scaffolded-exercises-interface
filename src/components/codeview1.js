f4ddf0b82a183c522e3b222a8cdcc98eda1cc0a3
Maxine Whitely <m.whitely16@gmail.com>
2019-11-26 17:47:19 -0600

    finished onclick value highlighting. WiP: tutorons style info popups for js/css

--- src/components/codeview1.js
+++ src/components/codeview1.js
@@ -4,15 +4,32 @@ import './styles/codeview.css';
 window.$ = $;
 
 export default class Codeview1 extends React.Component {
-  inputVal(elem, str) {
-    elem.value = 'hi';
-    if ($('#position-0')[0] !== undefined) {
-      let positionVals = $('#position-0')[0].innerText.match(/\[(.*?) \]/);
-      let position0 = positionVals
-      let position1 = positionVals
-      if (str === 'position[0]') return position0;
-      if (str === 'position[1]') return position1;
+  clickToInspectCode(e) {
+    if (e.target.id === 'position0-code-display') {
+      $('#position-0')[0].style.backgroundColor = 'yellow';
+      $('#position0-code-display')[0].style.backgroundColor = 'yellow';
+      $('#position0-code-display')[0].style.color = 'black';
+      setTimeout(function () {
+        $('#position-0')[0].style.backgroundColor = 'initial';
+        $('#position0-code-display')[0].style.backgroundColor = '#0000ff8a';
+        $('#position0-code-display')[0].style.color = 'white';
+      }, 2000);
     }
+
+    if (e.target.id === 'position1-code-display') {
+      $('#position-0')[0].style.backgroundColor = 'yellow';
+      $('#position1-code-display')[0].style.backgroundColor = 'yellow';
+      $('#position1-code-display')[0].style.color = 'black';
+      setTimeout(function () {
+        $('#position-0')[0].style.backgroundColor = 'initial';
+        $('#position1-code-display')[0].style.backgroundColor = '#0000ff8a';
+        $('#position1-code-display')[0].style.color = 'white';
+      }, 2000);
+    }
+  }
+
+  clickToInspectDocs(e) {
+
   }
 
   render() {
@@ -22,12 +39,15 @@ export default class Codeview1 extends React.Component {
           {`$map.css({`}
           <br></br>
           &nbsp;&nbsp;
-          <span className="tutorons-code-inspect">left</span>: 
-          <span className="var-value-code-inspect">position[0]</span>,
+          <span className="tutorons-code-inspect">left
+            <span class="tutorons-text">The left CSS property participates in specifying the horizontal position of a positioned element. It has no effect on non-positioned elements.</span> 
+            {/* will use the <meta name="description" content=""> content values for this to do it programmatically on https://developer.mozilla.org/en-US/docs/Web/CSS (AND/OR PLY) */}
+          </span>: 
+          <span className="var-value-code-inspect" id="position0-code-display" onClick={this.clickToInspectCode}>position[0]</span>,
           <br></br>
           &nbsp;&nbsp;
           <span className="tutorons-code-inspect">top</span>:
-          <span className="var-value-code-inspect" onLoad={this.inputVal(this, 'position[1]')}>position[1]</span>,
+          <span className="var-value-code-inspect" id="position1-code-display" onClick={this.clickToInspectCode}>position[1]</span>,
           <br></br>
           {`})`}
         </div>
