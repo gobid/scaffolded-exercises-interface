ed7ae6eef600448a9a59d17885528544a3f24537
Maxine Whitely <m.whitely16@gmail.com>
2019-12-03 16:52:22 -0600

    fix code question pop-out

--- src/App.js
+++ src/App.js
@@ -185,7 +185,7 @@ export default class App extends React.Component {
     /* Opens code related to reflection questions */
     $('#reflection-q-code-chevron').click(() => {
       let modalDisplay = $('#codeview1')[0].style.display;
-      (modalDisplay === "none") ? modalDisplay = "block" : modalDisplay = "none";
+      (modalDisplay === "none" || modalDisplay === "") ? modalDisplay = "block" : modalDisplay = "none";
       $('#codeview1')[0].style.display = modalDisplay;
 
       let chevronDir = $('#reflection-q-code-chevron')[0].innerText;
